0b983261ffcfdd2c328cbc2d83b24680de7d7c07
Peter Rhodes <rhodespeter@hotmail.co.uk>
2017-05-28 22:48:46 +0100

    Pause Game when you run into an obstacle.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 
 document.onkeydown = checkKey;
 var player = document.getElementsByClassName('game__player')[0];
+var obstacles = document.getElementsByClassName('obstacle');
 
 function checkKey(e) {
   if (e.keyCode == '38') {
@@ -11,7 +12,24 @@ function checkKey(e) {
   }
   setTimeout(function () {
     player.style.transform = 'translate(20px, 33px)';
-  }, 200);
+  }, 230);
+}
+
+setInterval(function () {
+  var playerPos = player.getBoundingClientRect();
+  for(var i = 0; i < obstacles.length; i++) {
+    var obstaclePos = obstacles[i].getBoundingClientRect();
+    if (obstaclePos.left > playerPos.left && obstaclePos.left < playerPos.right){
+      if (obstaclePos.top <= playerPos.bottom){
+        gameOver();
+      }
+    }
+  }
+}, 30);
+
+function gameOver (element) {
+  document.getElementsByClassName('game__ground')[0].style.webkitAnimationPlayState='paused';
+  console.log('GAME OVER!');
 }
 
 })();
