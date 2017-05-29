f811a7ec5f3f30bc4307a281d0f7cd296b817766
Peter Rhodes <rhodespeter@hotmail.co.uk>
2017-05-29 22:45:57 +0100

    Accelerate ground speed

--- index.js
+++ index.js
@@ -5,17 +5,30 @@
 document.onkeydown = checkKey;
 var player = document.getElementsByClassName('game__player')[0];
 var obstacles = document.getElementsByClassName('obstacle');
+var ground = document.getElementsByClassName('game__ground')[0];
+var playerJumping = false;
+var isGameOver = false;
+var gameOngoing = false;
+var increaseSpeed = null;
 
 function checkKey(e) {
-  if (e.keyCode == '38') {
-    player.style.transform = 'translate(20px, -20px)';
+  if (e.keyCode === 38) {
+    startGame();
+    playerJump();
   }
-  setTimeout(function () {
-    player.style.transform = 'translate(20px, 33px)';
-  }, 230);
 }
 
-setInterval(function () {
+function controlGameSpeed() {
+  var seconds = 30;
+  increaseSpeed = setInterval(function () {
+    if (seconds > 19){
+      seconds -= .0035;
+      ground.style.webkitAnimationDuration = seconds + 's';
+    }
+  }, 10);
+}
+
+var checkPlayerPos = setInterval(function () {
   var playerPos = player.getBoundingClientRect();
   for(var i = 0; i < obstacles.length; i++) {
     var obstaclePos = obstacles[i].getBoundingClientRect();
@@ -25,10 +38,34 @@ setInterval(function () {
       }
     }
   }
-}, 30);
+}, 10);
+
+function startGame() {
+  if (!isGameOver && !gameOngoing){
+    ground.style.webkitAnimationPlayState = 'running';
+    controlGameSpeed();
+    gameOngoing = true;
+  }
+}
+
+function playerJump () {
+  if (!playerJumping){
+    playerJumping = true;
+    player.style.transform = 'translate(20px, -20px)';
+    setTimeout(function () {
+      player.style.transform = 'translate(20px, 33px)';
+      setTimeout(function () {
+        playerJumping = false;
+      }, 190);
+    }, 230);
+  }
+}
 
 function gameOver (element) {
-  document.getElementsByClassName('game__ground')[0].style.webkitAnimationPlayState='paused';
+  ground.style.webkitAnimationPlayState = 'paused';
+  clearInterval(checkPlayerPos);
+  clearInterval(increaseSpeed);
+  isGameOver = true;
   console.log('GAME OVER!');
 }
 
